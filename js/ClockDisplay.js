79a71b114f101927460d17b4ae0cc1e52f98e68c
Daniel Blum <drumblum@gmail.com>
2019-10-23 13:53:28 -0700

    making clock stupid

--- js/ClockDisplay.js
+++ js/ClockDisplay.js
@@ -2,43 +2,43 @@
  * @file ClockDisplay.js
  */
 
-const clockDisplay = () => {
-  update();
-  window.setInterval(update, 1000);
-};
-
-const update = () => {
-  const dateOutput = $('#date');
-  const timeOutput = $('#time');
-
-  const months = ["January","February","March","April","May","June","July",
-    "August","September","October","November","December"];
-  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
-    "Saturday"];
-
-  const date = new Date();
-
-  let ampm = date.getHours() < 12 ? 'AM':'PM';
-  let hours = date.getHours() === 0
-    ? 12 : date.getHours() > 12
-    ? date.getHours() - 12 : date.getHours();
-  let minutes = date.getMinutes() < 10
-    ? '0' + date.getMinutes() : date.getMinutes();
-  let seconds = date.getSeconds() < 10
-    ? '0' + date.getSeconds() : date.getSeconds();
-
-  let dayOfWeek = days[date.getDay()];
-  let month = months[date.getMonth()];
-  let day = date.getDate();
-  let year = date.getFullYear();
-
-  const dateString = dayOfWeek + ', ' + month + " " + month + " " + day + ", "
-    + year;
-  dateOutput.text(dateString);
-
-  const timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
-  timeOutput.text(timeString);
-};
-
-export default clockDisplay;
+// const clockDisplay = () => {
+//   console.log("in clockDisplay()");
+//
+//   const dateOutput = $('#date');
+//   const timeOutput = $('#time');
+//
+//   const months = ["January","February","March","April","May","June","July",
+//     "August","September","October","November","December"];
+//   const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
+//     "Saturday"];
+//
+//   const date = new Date();
+//
+//   let ampm = date.getHours() < 12 ? 'AM':'PM';
+//   let hours = date.getHours() === 0
+//       ? 12 : date.getHours() > 12
+//           ? date.getHours() - 12 : date.getHours();
+//   let minutes = date.getMinutes() < 10
+//       ? '0' + date.getMinutes() : date.getMinutes();
+//   let seconds = date.getSeconds() < 10
+//       ? '0' + date.getSeconds() : date.getSeconds();
+//
+//   let dayOfWeek = days[date.getDay()];
+//   let month = months[date.getMonth()];
+//   let day = date.getDate();
+//   let year = date.getFullYear();
+//
+//   const dateString = dayOfWeek + ', ' + month + " " + month + " " + day + ", "
+//       + year;
+//   dateOutput.text(dateString);
+//
+//   const timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
+//   timeOutput.text(timeString);
+//
+//
+//   window.setInterval(clockDisplay, 1000);
+// };
+//
+// export default clockDisplay;
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
-import clockDisplay from './ClockDisplay'
-import keyDisplay from './KeyDisplay'
+
 
 window.onload = function()
 {
+  console.log("today junior");
   clockDisplay();
-  // keyDisplay();
+};
+
+const clockDisplay = () => {
+  console.log("in clockDisplay()");
+
+  const dateOutput = $('#date');
+  const timeOutput = $('#time');
+
+  const months = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday",
+    "Saturday"];
+
+  const date = new Date();
+
+  let ampm = date.getHours() < 12 ? 'AM':'PM';
+  let hours = date.getHours() === 0
+      ? 12 : date.getHours() > 12
+          ? date.getHours() - 12 : date.getHours();
+  let minutes = date.getMinutes() < 10
+      ? '0' + date.getMinutes() : date.getMinutes();
+  let seconds = date.getSeconds() < 10
+      ? '0' + date.getSeconds() : date.getSeconds();
+
+  let dayOfWeek = days[date.getDay()];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+  let year = date.getFullYear();
+
+  const dateString = dayOfWeek + ', ' + month + " " + month + " " + day + ", "
+      + year;
+  dateOutput.text(dateString);
+
+  const timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
+  timeOutput.text(timeString);
+
+
+  window.setInterval(clockDisplay, 1000);
 };
 
